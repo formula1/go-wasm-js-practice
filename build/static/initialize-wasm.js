7ab952613d4c6576b7eaf2adc7e643a877b54c49
Samuel Tobia <sam@system76-pc.localdomain>
2023-02-26 13:43:11 -0800

    UX-Feat: added needed files to interact with the compiled wasm file

---
+++ build/static/initialize-wasm.js
@@ -0,0 +1,18 @@
+
+if (WebAssembly) {
+// WebAssembly.instantiateStreaming is not currently available in Safari
+  if (WebAssembly && !WebAssembly.instantiateStreaming) { // polyfill
+    WebAssembly.instantiateStreaming = async (resp, importObject) => {
+      const source = await (await resp).arrayBuffer();
+      return await WebAssembly.instantiate(source, importObject);
+    };
+  }  
+
+  const go = new Go();
+  WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
+    go.run(result.instance);
+  });
+} else {
+  console.log("WebAssembly is not supported in your browser")
+}
+
